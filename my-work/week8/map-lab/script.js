f56557af7509bb0a370591e3c0865479f3295824
QianyiChen532 <qc532@nyu.edu>
2020-04-20 13:20:53 +0800

    map-issue2

--- my-work/week8/map-lab/script.js
+++ my-work/week8/map-lab/script.js
@@ -3,11 +3,12 @@ let h = 800;
 let padding = 90
 
 let mapIndex;
+let mapData;
 // SVG
 let viz = d3.select("#container").append("svg")
-    .style("width", w)
-    .style("height", h)
-    .style("background-color", "lavender")
+.style("width", w)
+.style("height", h)
+.style("background-color", "lavender")
 ;
 
 let projections = [
@@ -27,112 +28,174 @@ d3.json("mainland.geojson").then(function(geoData){
   d3.csv("china-pop-2018.csv").then(function(incomingData){
     d3.json("countries.geojson").then(function(countryData){
 
-    incomingData = incomingData.map(function(d,i){
-      d.population = Number(d.population);
-      return d;
-    });
-    // console.log(incomingData);
+      console.log(geoData);//latitude longitude
 
-    let minPop = d3.min(incomingData, function(d,i){
-      return d.population;
-    });
-    console.log("minPop", minPop);
-    let maxPop = d3.max(incomingData, function(d,i){
-      return d.population;
-    });
-    // console.log("maxPop", maxPop);
+      incomingData = incomingData.map(function(d,i){
+        d.population = Number(d.population);
+        return d;
+      });
+      // console.log(incomingData);
 
-
-    let colorScale = d3.scaleLinear().domain([minPop, maxPop]).range(["white", "#237d94"]);
-    console.log(colorScale(20));
+      let minPop = d3.min(incomingData, function(d,i){
+        return d.population;
+      });
+      console.log("minPop", minPop);
+      let maxPop = d3.max(incomingData, function(d,i){
+        return d.population;
+      });
+      // console.log("maxPop", maxPop);
 
 
-
-
-    let mapGroup = viz.append("g").attr("class", "mapGroup");
-
-    function updateMap(mapData){
-
-      if (mapData == countryData){
-        mapIndex =0;
-      }else{
-        mapIndex = 1;
-      }
+      let colorScale = d3.scaleLinear().domain([minPop, maxPop]).range(["white", "#237d94"]);
+      console.log(colorScale(20));
 
       let projection = d3.geoEqualEarth()
-        .translate([w/2, h/2])
-        .fitExtent([[padding, padding], [w-padding, h-padding]], mapData);
-
-      let pathMaker = d3.geoPath()
-      .projection(projection);
+      .translate([w/2, h/2])
 
 
-      let map = mapGroup.selectAll('.countries').data(mapData.features)
-      console.log(map);
+      var svg = d3.select('svg');
 
-      let enteringElement = map.enter();
-      let exitingElement = map.exit();
+      var rect3dGroup = svg.append('g')
+      .attr('class','rect3dGroup')
+      // .attr("transform", "translate (50,50)")
+      ;
+      let rect3d  =rect3dGroup.selectAll('rect')
+      .data(geoData.features)
+      .enter()
 
-      enteringElement
-      .append('path')
-      // .transition()
-      // .duration(500)
-      .attr('d',pathMaker)
-      .attr('class','countries')
-      .attr('fill','black')
-      .attr('stroke','white')
-      .attr('stroke-width','1')
       ;
 
-      //update, don't do enter()
-      map
-      .transition()
-      .duration(500)
-        .attr("d", pathMaker)
-        ;
+      var rh = 20, rw = 20, ang=45;
 
-      exitingElement
-      .transition()
-      .duration(500)
-      .remove()
+      rect3d.append("rect")
+      .attr('class','rect3d')
+      .attr("x", function(d,i){
+        let lat = d.properties.latitude;
+        let lon = d.properties.longitude;
+        // console.log(projection([lon, lat]));
+        return projection([lon, lat])[0];
+      })
+      .attr("y", function(d,i){
+        let lat = d.properties.latitude;
+        let lon = d.properties.longitude;
+
+        return projection([lon, lat])[1];
+      })
+      .attr("width", rw)
+      .attr("height", rh)
+      .attr('fill','white')
+
       ;
 
 
-    }
+      // rect3d.append("rect")
+      // .attr('class','rect3d')
+      // .attr("x", function(d,i){
+      //   return d.properties.latitude;
+      // })
+      // .attr("y", function(d,i){
+      //   return d.properties.longitude;
+      // })
+      // .attr("width", rw)
+      // .attr("height", rh/2)
+      // .attr ("transform", "translate ("+(-rh/2)+","+(-rh/2)+") skewX("+ang+")")
+      // ;
+      //
+      // rect3d.append("rect")
+      //   .attr("class", "side")
+      //  .attr("x", 0)
+      // .attr("y", 0)
+      // .attr("width", rh/2)
+      // .attr("height", rh)
+      // .attr ("transform", "translate ("+(-rh/2)+","+(-rh/2)+") skewY("+ang+")")
+      // ;
 
-    updateMap(countryData);//world map by default
 
-    function changeProjection(mapData){
-      // if(mapIndex == 0){
-      //   mapData = countryData;
-      // }else {
-      //   mapData = geoData;
-      // }
-      mapData = countryData;
-
-      let n = Math.floor(Math.random()*projections.length);
-      console.log(projections[n].name);
-
-      projection = projections[n].projection
+      projection= d3.geoEqualEarth()
       .translate([w/2, h/2])
       .fitExtent([[padding, padding], [w-padding, h-padding]], mapData);
 
-      updateMap(mapData);
 
-    }
+      let mapGroup = viz.append("g").attr("class", "mapGroup");
 
-    document.getElementById("projection").addEventListener("click", changeProjection);
+      function updateMap(mapData){
 
-    document.getElementById("map-cn").addEventListener("click",function(){
-      if(mapIndex == 0){
-        updateMap(geoData);
-      }else{
-        updateMap(countryData);
+        if (mapData == countryData){
+          mapIndex =0;
+        }else{
+          mapIndex = 1;
+        }
+
+
+        let pathMaker = d3.geoPath()
+        .projection(projection);
+
+
+        let map = mapGroup.selectAll('.countries').data(mapData.features)
+        console.log(map);
+
+        let enteringElement = map.enter();
+        let exitingElement = map.exit();
+
+
+        enteringElement
+        .append('path')
+        // .transition()
+        // .duration(500)
+        .attr('d',pathMaker)
+        .attr('class','countries')
+        .attr('fill','black')
+        .attr('stroke','white')
+        .attr('stroke-width','1')
+        ;
+
+        //update, don't do enter()
+        map
+        .transition()
+        .duration(500)
+        .attr("d", pathMaker)
+        ;
+
+        exitingElement
+        .transition()
+        .duration(500)
+        .remove()
+        ;
+
+
       }
-      console.log(mapIndex);
 
-    });
+      updateMap(countryData);//world map by default
 
+      function changeProjection(mapData){
+        if(mapIndex == 0){
+          mapData = countryData;
+        }else {
+          mapData = geoData;
+        }
+        mapData = countryData;
+
+        let n = Math.floor(Math.random()*projections.length);
+        console.log(projections[n].name);
+
+        projection = projections[n].projection
+        .translate([w/2, h/2])
+        .fitExtent([[padding, padding], [w-padding, h-padding]], mapData);
+        updateMap(mapData);
+      }
+
+      document.getElementById("projection").addEventListener("click", changeProjection);
+
+      document.getElementById("map-cn").addEventListener("click",function(){
+        if(mapIndex == 0){
+          updateMap(geoData);
+        }else{
+          updateMap(countryData);
+        }
+        console.log(mapIndex);
+
+      });
+
+    })
   })
-})
 })
